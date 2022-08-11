6658af48cd1555fb91d06887e5bd519dba7ad5e3
 Mekala Vamsi Krishna <68817698+mekala-vamsi-krishna@users.noreply.github.com>
2022-08-11 15:11:46 +0530

    Add files via upload

---
+++ 14. ByteCoin-iOS13/ByteCoin/Model/CoinData.swift
@@ -0,0 +1,13 @@
+//
+//  coinData.swift
+//  ByteCoin
+//
+//  Created by Mekala Vamsi Krishna on 17/07/22.
+//  Copyright © 2022 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct CoinData: Codable {
+    let rate: Double
+}
